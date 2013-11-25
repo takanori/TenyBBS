f230ba8ef95c5ca3bbeea61462e1bbeaf0563988
pacocat <paco.sci@gmail.com>
2013-11-25 12:01:13 +0900

    temporal version

--- lib/TenyBBS/DB/Schema.pm
+++ lib/TenyBBS/DB/Schema.pm
@@ -8,9 +8,15 @@ use Teng::Schema::Declare;
 base_row_class 'TenyBBS::DB::Row';
 
 table {
-    name 'member';
+    name 'threads';
     pk 'id';
-    columns qw(id name);
+    columns qw(id author_name title content created_at updated_at);
+};
+
+table {
+    name 'entries';
+    pk 'id';
+    columns qw(id thread_id author_name content created_at updated_at);
 };
 
 1;
--- lib/TenyBBS/Web/Dispatcher.pm
+++ lib/TenyBBS/Web/Dispatcher.pm
@@ -6,13 +6,20 @@ use Amon2::Web::Dispatcher::RouterBoom;
 
 any '/' => sub {
     my ($c) = @_;
-    return $c->render('index.tx');
+    my @thread_list = $c->db->fetch_threads;
+    my @entry_list = $c->db->fetch_entries;
+    return $c->render('index.tx', { thread_list => \@thread_list, entry_list => \@entry_list});
 };
 
-post '/account/logout' => sub {
+
+post '/thread/insert' => sub {
     my ($c) = @_;
-    $c->session->expire();
+    my $title = $c->req->param('title');
+    my $content = $c->req->param('content');
+    my $author_name = $c->req->param('author_name');
+    $c->db->create_thread($title, $content, $author_name);
     return $c->redirect('/');
 };
+
 
 1;
